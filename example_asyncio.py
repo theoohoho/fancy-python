cc9cc390804a26222ac6b23b6fa456cb028bc268
theoohoho <topslimer@gmail.com>
2021-08-30 17:13:13 +0800

    Format code

--- example_asyncio.py
+++ example_asyncio.py
@@ -6,12 +6,13 @@ Simple async task
 # get event loop
 async_loop = asyncio.get_event_loop()
 
-print('\n========Simple example===========\n')
+print("\n========Simple example===========\n")
 # create coroutine function
 async def foo(num):
-    print(f'# {num} First phase invoke')
+    print(f"# {num} First phase invoke")
     await asyncio.sleep(2)
-    print(f'# {num} Second phase invoke')
+    print(f"# {num} Second phase invoke")
+
 
 # register coroutine functioni into eveny loop and running
 async_loop.run_until_complete(foo(1))
@@ -20,7 +21,7 @@ async_loop.run_until_complete(foo(1))
 """
 Multi task
 """
-print('\n========Multi task===========\n')
+print("\n========Multi task===========\n")
 # create muitl task
 tasks = [asyncio.ensure_future(foo(i)) for i in range(5)]
 
@@ -33,7 +34,9 @@ async_loop.close()
 """
 Python 3.7 support
 """
-print('\n========Python3.7 support Multi task===========\n')
+print("\n========Python3.7 support Multi task===========\n")
+
+
 async def run_multi_tasks():
     task1 = asyncio.create_task(foo(1))
     task2 = asyncio.create_task(foo(2))
@@ -41,5 +44,6 @@ async def run_multi_tasks():
     await task1
     await task2
     await task3
-    
+
+
 asyncio.run(run_multi_tasks())
--- example_threading.py
+++ example_threading.py
@@ -3,6 +3,7 @@
 import threading
 import time
 
+
 def foo(number: int):
     # create lock instance
     # 針對 multi thread，每個 thread 在同時進行時，如果針對某些不能同時操作的事情時 e.g database寫入、寫入同一個檔案
@@ -11,13 +12,15 @@ def foo(number: int):
     # acquire lock control
     lock.acquire()
     # logic
-    for i in range(number): print(i)
-    print('finished foo() logic')
+    for i in range(number):
+        print(i)
+    print("finished foo() logic")
     # release lock control
     lock.release()
 
+
 """
-regular 
+regular
 """
 sample_thread = threading.Thread(target=foo, args=10)
 # run threading
@@ -26,7 +29,7 @@ sample_thread.start()
 # 主要針對某些需要等待 thread 跑完才能執行的邏輯
 sample_thread.join()
 # after thread finished, continous run later logic
-print('Done.')
+print("Done.")
 
 
 """ 
@@ -41,12 +44,14 @@ for i in range(5):
     threads[i].join()
 
 # after thread finished, continous run later logic
-print('Done.')
+print("Done.")
 
 
 """
 defined custom Thread class
 """
+
+
 class Worker(threading.Thread):
     def __init__(self, tag: int):
         threading.Thread.__init__(self)
@@ -57,10 +62,11 @@ class Worker(threading.Thread):
         foo(self.tag)
         time.sleep(1)
 
+
 for i in range(3):
     worker = Worker(i)
     worker.start()
     worker.join()
 
 # after thread finished, continous run later logic
-print('Done.')
+print("Done.")
